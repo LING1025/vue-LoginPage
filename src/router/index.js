59b91aefbaa07241c631dbbc088b8eebee4783a0
justone <2274104495@qq.com>
2020-04-17 17:12:02 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '@/views/Main'
+import Login from '@/views/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      name: 'Login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'Main',
+      path: '/main',
+      component: Main
+    }
+  ]
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1 class="login-title">欢迎登录</h1>
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px" class="login-box">
+      <el-form-item label="账号" prop="username">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login('form')">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login.vue',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          {required: true, message: '请输入账号', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    login: function (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$router.push('/main')
+        } else {
+          this.$message.error('请输入账号密码')
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login-title{
+    text-align: center;
+  }
+
+  .login-box{
+    text-align: center;
+    width: 400px;
+    border: 1px solid black;
+    margin: 0 auto;
+    margin-top: 150px;
+    padding: 20px 50px 20px 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px;
+  }
+</style>
